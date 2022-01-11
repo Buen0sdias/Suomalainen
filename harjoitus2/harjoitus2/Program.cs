5ec37e7ad10b45d2afbeb5da96755913e6ee9370
Steven Suomalainen <Steven.Suomalainen@student.careeria.fi>
2022-01-11 13:37:35 +0200

    upload

---
+++ harjoitus2/harjoitus2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace harjoitus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija2 = new Opiskelija("matti", "TVT1234", 0);
+
+            opiskelija2.TulostaData();
+            opiskelija2.MuokkaaOpintoPisteitä(5);
+            opiskelija2.TulostaData();
+        }
+    }
+}
