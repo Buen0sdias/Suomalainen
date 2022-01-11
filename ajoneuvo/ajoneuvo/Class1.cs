ace1bb68cd11d66055d04538152aff8247bed76f
Suomalainen Steven <steven.suomalainen@student.careeria.fi>
2022-01-11 11:04:05 +0200

    ajoneuvoharjoituksenlisäys

---
+++ ajoneuvo/ajoneuvo/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ajoneuvo
+{
+    class Ajoneuvo
+    {
+        public string Nimi { get; set; }
+
+        public int Nopeus { get; set; }
+
+        public int Renkaat { get; set; }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Ajoneuvo ");
+            Console.WriteLine("--Nimi: " + Nimi);
+            Console.WriteLine("--Nopeus:" + Nopeus);
+            Console.WriteLine("--renkaat:" + Renkaat);
+
+        }
+
+        //public string ToString()
+        //{
+
+        //}
+
+
+    }
+}
---
+++ ajoneuvo/ajoneuvo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ajoneuvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo auto = new Ajoneuvo();
+
+            auto.Nimi = "Toyota";
+            auto.Nopeus = 160;
+            auto.Renkaat = 4;
+
+            auto.TulostaData();
+
+            string AutonTiedot = auto.ToString();
+            Console.WriteLine(AutonTiedot);
+
+
+
+            Ajoneuvo mopo = new Ajoneuvo();
+            mopo.Nimi = "Skootteri";
+            mopo.Nopeus = 55;
+            mopo.Renkaat = 2;
+
+            mopo.TulostaData();
+            Console.Write(mopo.ToString());
+        }
+    }
+}
