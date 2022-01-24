6eb2a8e6b5a5780d506a0bc6c4f9fc7251115c5c
Steven Suomalainen <Steven.Suomalainen@student.careeria.fi>
2022-01-24 13:47:14 +0200

    up

---
+++ Kilometri/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kilometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Kilometri = 60f;
+
+           float mailit = Staattinenluokka.KmToMiles(Kilometri);
+
+            Console.WriteLine("{0}km/h on yhtäkuin {1} mailia", Kilometri, mailit);
+        }
+    }
+}
---
+++ Kilometri/Staattinenluokka.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kilometri
+{
+    //staattinen luokka
+    static class Staattinenluokka
+    {
+        public static float KmToMiles(float i)
+        {
+            float conversion = 0.62137f;
+
+            return i * conversion;
+        }
+
+
+
+            
+    }
+}
