93dc6c38dc72d1b0c9129f70efda4ae89e009e94
Steven Suomalainen <Steven.Suomalainen@student.careeria.fi>
2022-01-24 10:51:38 +0200

    harvia

---
+++ harvia1/harvia1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace harvia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kiuas kiuas1 = new Kiuas("Harvia", 0);
+
+
+
+            kiuas1.onOFF();
+
+
+
+
+            kiuas1.MuokkaaKosteutta(10);
+            kiuas1.MuokkaaLämpötilaa(120);
+            kiuas1.Tulostadata();
+            kiuas1.onOFF();
+
+
+
+
+
+
+
+        }
+    }
+}
