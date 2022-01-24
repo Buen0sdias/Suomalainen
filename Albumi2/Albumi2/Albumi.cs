381476a32898e3dab9eeaa757a45b48178b8c478
Steven Suomalainen <Steven.Suomalainen@student.careeria.fi>
2022-01-24 11:06:37 +0200

    upload

---
+++ Albumi2/Albumi2/Albumi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Albumi2
+{
+    class Kappale
+
+    {
+        private IList cities = new List<string>();
+
+
+    }
+}
---
+++ Albumi2/Albumi2/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Albumi2
+{
+    class Albumi
+
+    {
+
+
+        public string Artisti { get; set; }
+        public string Nimi { get; set; }
+        public string Genre { get; set; }
+        public string Hinta { get; set; }
+        public string Kappaleet { get; set; }
+
+
+
+        List<string> PriceList = new List<string>(5);
+
+
+        public void Tulostakappaleet()
+        {
+            PriceList.Add("Kisssa");
+            PriceList.Add("Joonatan");
+            PriceList.Add("Romeo ja julia");
+            PriceList.Add("Keisari");
+            PriceList.Add("Nakki Bileet");
+        }
+
+
+        public void Tulostadata()
+        {
+            Console.WriteLine("Albumi");
+            Console.WriteLine("--- Artisti:" + Artisti);
+            Console.WriteLine("--- Nimi:" + Nimi);
+            Console.WriteLine("---Genre:" + Genre);
+            Console.WriteLine("---Hinta:" + Hinta);
+            Console.WriteLine("---Kappaleet:" + Kappaleet);
+            PriceList.Sort();
+            PriceList.ForEach(Console.WriteLine);
+        }
+
+
+
+    }
+}
