de9895db806f7fe0ee99c584b5790b003ff7bba7
Steven Suomalainen <Steven.Suomalainen@student.careeria.fi>
2022-01-25 12:12:29 +0200

    alien22

---
+++ hissi/hissi/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vahvistin
+{
+    class vahvistin
+    {
+        private int äänenvoimakkuus;
+
+
+        public int Äänenvoimakkuus1
+        {
+            get => äänenvoimakkuus;
+            set
+            {
+                if (value > 6)
+                {
+                    Console.WriteLine("Error virheellinen kerros!");
+                    value = 6;
+                }
+                else if (value < 0)
+                {
+                    Console.WriteLine("Anna uusi kerros");
+                    value = 0;
+                }
+
+                Console.WriteLine("Hissi on nyt kerroksessa " + value);
+
+                äänenvoimakkuus = value;
+            }
+
+        }
+    }
+}
+
---
+++ hissi/hissi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Vahvistin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uusiVoimakkuus;
+
+            Console.WriteLine("Hissi..");
+            vahvistin vahvistin = new vahvistin();
+            Console.WriteLine("Hissi on tullut 'Sammuta' ");
+
+            while (true)
+            {
+                Console.WriteLine("Anna uusi kerros (1-6) > ");
+                string LuettuArvo = Console.ReadLine();
+
+                if (LuettuArvo.Equals("Sammuta"))
+                {
+                    break; //Poistutaan loopista
+
+                }
+
+                bool result = int.TryParse(LuettuArvo, out uusiVoimakkuus);
+
+                if (result)
+                {
+                    vahvistin.Äänenvoimakkuus1 = int.Parse(LuettuArvo);
+                }
+                else
+                {
+                    Console.WriteLine("Error: Annettu arvo on virheellinen: Mikäli haluat sammuttaa hissin kirjoita: Sammuta");
+
+                }
+            }
+
+            Console.WriteLine("hissi on sammutettu");
+        }
+    }
+}
