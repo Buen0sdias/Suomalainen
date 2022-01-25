fb458c9f6963833ed9d1aa2ee23a2ac40c118b9c
Steven Suomalainen <Steven.Suomalainen@student.careeria.fi>
2022-01-25 09:54:31 +0200

    alien22

---
+++ Vahvistin/Vahvistin/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Vahvistin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uusiVoimakkuus;
+
+            Console.WriteLine("Luodaan uusi vahvistin..");
+            vahvistin vahvistin = new vahvistin();
+            Console.WriteLine("Uusi vahvistin luotu, poistu kirjoittamalla 'Sammuta' ");
+
+            while(true)
+            {
+                Console.WriteLine("Anna uusi äänenvoimakkuus vahvistimelle (0-100) > ");
+                string LuettuArvo = Console.ReadLine();
+
+                if (LuettuArvo.Equals("Sammuta"))
+                {
+                    break; //Poistutaan loopista
+
+                }
+
+                bool result = int.TryParse(LuettuArvo, out uusiVoimakkuus);
+
+                if (result)
+                {
+                    vahvistin.Äänenvoimakkuus1 = int.Parse(LuettuArvo);
+                }
+                else
+                {
+                    Console.WriteLine("Error: Annettu arvo on virheellinen: Mikäli haluat sammuttaa vahvistimen kirjoita: Sammuta");
+
+                }
+            }
+
+            Console.WriteLine("vahvistin on sammutettu");
+        }
+    }
+}
---
+++ Vahvistin/Vahvistin/vahvistin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vahvistin
+{
+    class vahvistin
+    {
+        private int äänenvoimakkuus;
+
+           
+        public int Äänenvoimakkuus1 { 
+         get => äänenvoimakkuus;
+            set
+            {
+                if ( value > 100)
+                {
+                    Console.WriteLine("Annettu arvo oli yli 100");
+                    value = 100;
+                } else if (value < 0)
+                {
+                    Console.WriteLine("Annettu arvo oli pienempi kuin 0");
+                    value = 0;
+                }
+
+                Console.WriteLine("Äänenvoimakkuus asetettu " + value);
+
+                äänenvoimakkuus = value;
+            }
+            
+        }
+    }
+}
