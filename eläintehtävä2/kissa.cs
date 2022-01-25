d0c7d33218f7a1082bc729ca77da6cb249d8fd62
Steven Suomalainen <Steven.Suomalainen@student.careeria.fi>
2022-01-25 09:20:02 +0200

    alien22

---
+++ eläintehtävä2/kissa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tehävä6Eläin
+{
+    class Kissa : Eläin
+    {
+        private static int instanssit = 0;
+
+        public Kissa()
+        {
+
+            instanssit++;
+
+        }
+        public static new void KuinkaMonta()
+        {
+            Console.WriteLine("Kissa luotu " + instanssit);
+        }
+        public override void Ääntele()
+        {
+            Console.WriteLine("Meeeeeeoow Meeeeeooow");
+        }
+    }
+}
---
+++ eläintehtävä2/koira.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tehävä6Eläin
+{
+    //eläimen aliluokka
+    class Koira : Eläin
+    {
+
+        private static int instanssit = 0;
+
+        public Koira()
+        {
+
+            instanssit++;
+
+        }
+        public static new void KuinkaMonta()
+        {
+
+            Console.WriteLine("Koira luotu " + instanssit);
+        }
+
+        public override void Ääntele()
+        {
+
+            Console.WriteLine("Wufffff wuuuuffff");
+
+        }
+    }
+}
